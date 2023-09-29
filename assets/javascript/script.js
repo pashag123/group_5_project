e7484cdf96fe3a53c7dc6632848d2dcaa50b3aa6
amanda-er <amanda.er991@gmail.com>
2023-09-28 22:02:10 -0700

    pull from main

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,7 @@
-console.log('goodbye dps')
+fetch('www.thecocktaildb.com/api/json/v1/1/random.php')
+.then(function(response) {
+    return response.json()
+})
+.then(function(data) {
+    console.log(data)
+})
