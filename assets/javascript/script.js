265503419a9c4008dbaf5a8b112782c9bf62e2df
GaryM636 <142124124+GaryM636@users.noreply.github.com>
2023-10-02 21:21:48 -0700

    added js classes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -43,7 +43,7 @@ function renderCards(movies) {
     <div class="card">
         <div class="card-image">
             <div>
-                <img src="">
+                <img src="https://image.tmdb.org/t/p/original/${movies[i].poster_path}">
             </div>
             
             <a class="btn-floating halfway-fab waves-effect waves-light red"><i
@@ -52,9 +52,8 @@ function renderCards(movies) {
 
         <div class="card-content">
             <div class="row">
-                <div class="col">${movies[i].release_date}</div>
-                <div class="col">${movies[i].vote_average}</div>
-                <div class="col">watch options</div>
+                <div class="col s6 m6 l6">${movies[i].release_date}</div>
+                <div class="col s6 m6 l6">${movies[i].vote_average}</div>
             </div>
             <h2>${movies[i].title}</h2>
             <p>${movies[i].overview}</p>
